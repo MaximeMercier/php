135970d50328ab3acfb924bac764265122623c89
MaximeMercier <38720310+MaximeMercier@users.noreply.github.com>
2018-08-31 12:43:49 +0200

    Update bdd.php

--- inc/bdd.php
+++ inc/bdd.php
@@ -3,7 +3,7 @@ require_once 'bdd.php';
 
 try {
     $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-    $bdd = new PDO('mysql:host=localhost;dbname=maximeme_php', 'maximeme_php', '@Root123', $pdo_options);
+    $bdd = new PDO('mysql:host=localhost;dbname=*****', '*****', '*****', $pdo_options);
     $bdd->exec("set names utf8");
 } catch (Exception $e) {
     die('Erreur : ' . $e->getMessage());
